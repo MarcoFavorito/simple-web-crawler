f96e51a6b561dfc6725a3a93ce3354ac53c87b02
Marco Favorito <marco.favorito@gmail.com>
2018-08-25 00:55:49 +0200

    basic implementation

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# simple-web-crawler
-A simple web crawler.
+# Simple Web Crawler
+
+![](https://img.shields.io/badge/python-2.7-blue.svg) ![](https://img.shields.io/badge/dep-scrapy-yellowgreen.svg) ![](https://img.shields.io/badge/dep-graphviz-lightgrey.svg)
+
+A very very simple web crawler. Take in input an URL and outputs the site map. It uses Scrapy and PyGraphviz.
+
+It ignores URLs of a different domain from the starting url.
+
+## Installation
+Please follow the installation guide of 
+[Scrapy](https://docs.scrapy.org/en/latest/intro/install.html#platform-specific-installation-notes)
+ and [Graphviz](https://www.graphviz.org/download/).
+
+Then, run the following:
+
+    pip install -r requirements.txt
+
+## Usage
+
+    $python2 simple-web-crawler.py -h
+    usage: simple-web-crawler.py [-h] [-o output_filename] url
+
+E.g.:
+
+    python2 simple-web-crawler.py https://www.lipsum.com/
+
+
+You can find the map in `output.svg` and in DOT format in `output`.
--- simple-web-crawler.py
+++ simple-web-crawler.py
@@ -1,13 +1,96 @@
 import argparse
+import re
+
+from graphviz import Digraph
+from scrapy import Selector
+from scrapy.crawler import CrawlerProcess
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
 
 parser = argparse.ArgumentParser(description='A very simple web crawler')
-parser.add_argument('url', metavar='URL', type=str, nargs=1,
+parser.add_argument('url', metavar='url', type=str,
                     help='The url of the page where to start.')
+parser.add_argument('-o', metavar='output_filename', dest="output_filename", type=str, default="output",
+                    help='The output filename.')
 
-args = parser.parse_args()
+arguments = parser.parse_args()
+graph = Digraph()
+
+
+def extract_domain(url):
+    try:
+        domain = next(re.finditer(":\/\/[^/]+", url)).group()[3:]
+        return domain
+    except Exception as e:
+        return None
+
+def extract_title(response):
+    try:
+        title = Selector(text=response.body).xpath('//title/text()').extract()[0]
+        title = title.strip()
+        return title
+    except Exception as e:
+        return ""
+
+
+def make_node_label(response):
+    return (extract_title(response) + "\n" + response.request.url).strip()
+
+
+def make_node_name(url):
+    # problems with Graphviz node names
+    name = "\"" + url + "\""
+    name = name.replace("://", ".")
+    return name
+
+class SimpleSpider(CrawlSpider):
+    name = 'simplespider'
+
+    rules = (
+        Rule(LinkExtractor(), callback='parse_item', follow=True,),
+    )
+
+    def parse_start_url(self, response):
+        graph.node(make_node_name(response.request.url), label=make_node_label(response))
+        request = super(SimpleSpider, self).parse_start_url(response)
+        return request
+
+    def _requests_to_follow(self, response):
+        for r in super(SimpleSpider, self)._requests_to_follow(response):
+            r.meta["parent_url"] = response.request.url
+            yield r
+
+
+    def parse_item(self, response):
+        current_url = make_node_name(response.request.url)
+        parent_url = make_node_name(response.meta["parent_url"])
+        # print "Parsed page: %s" % current_url, "Parent page: %s" % parent_url
+        graph.node(current_url, label=make_node_label(response))
+        graph.edge(parent_url, current_url)
+
 
 def main():
-    pass
+
+    start_url = arguments.url
+    domain = extract_domain(start_url)
+    if domain is None:
+        print "Error: Cannot find domain name in the url."
+        exit(1)
+
+    print "Starting url: %s" % start_url
+    print "Domain: %s" % domain
+
+    process = CrawlerProcess({
+        'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'
+    })
+
+    process.crawl(SimpleSpider, start_urls=[start_url], allowed_domains=[domain])
+    process.start()  # the script will block here until the crawling is finished
+
+    graph.graph_attr["rankdir"] = "LR"
+    graph.format = "svg"
+    graph.render(arguments.output_filename)
+
 
 
 if __name__ == '__main__':
